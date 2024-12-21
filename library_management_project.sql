0000671a88fdf9a2df79e06dc8fd89c2b166a84c
Abdirachman Mahamud <abdirachman.mxd@gmail.com>
2024-12-21 15:56:42 +0000

    Update library_management_project.sql

--- library_management_project.sql
+++ library_management_project.sql
@@ -1,5 +1,84 @@
 CREATE schema library_management;
+CREATE DATABASE library_management;
 
+DROP TABLE IF EXISTS branch;
+CREATE TABLE branch
+(
+            branch_id VARCHAR(10) PRIMARY KEY,
+            manager_id VARCHAR(10),
+            branch_address VARCHAR(30),
+            contact_no VARCHAR(15)
+);
+
+
+-- Create table "Employee"
+DROP TABLE IF EXISTS employees;
+CREATE TABLE employees
+(
+            emp_id VARCHAR(10) PRIMARY KEY,
+            emp_name VARCHAR(30),
+            position VARCHAR(30),
+            salary DECIMAL(10,2),
+            branch_id VARCHAR(10),
+            FOREIGN KEY (branch_id) REFERENCES  branch(branch_id)
+);
+
+
+-- Create table "Members"
+DROP TABLE IF EXISTS members;
+CREATE TABLE members
+(
+            member_id VARCHAR(10) PRIMARY KEY,
+            member_name VARCHAR(30),
+            member_address VARCHAR(30),
+            reg_date DATE
+);
+
+
+
+-- Create table "Books"
+DROP TABLE IF EXISTS books;
+CREATE TABLE books
+(
+            isbn VARCHAR(50) PRIMARY KEY,
+            book_title VARCHAR(80),
+            category VARCHAR(30),
+            rental_price DECIMAL(10,2),
+            status VARCHAR(10),
+            author VARCHAR(30),
+            publisher VARCHAR(30)
+);
+
+
+
+-- Create table "IssueStatus"
+DROP TABLE IF EXISTS issued_status;
+CREATE TABLE issued_status
+(
+            issued_id VARCHAR(10) PRIMARY KEY,
+            issued_member_id VARCHAR(30),
+            issued_book_name VARCHAR(80),
+            issued_date DATE,
+            issued_book_isbn VARCHAR(50),
+            issued_emp_id VARCHAR(10),
+            FOREIGN KEY (issued_member_id) REFERENCES members(member_id),
+            FOREIGN KEY (issued_emp_id) REFERENCES employees(emp_id),
+            FOREIGN KEY (issued_book_isbn) REFERENCES books(isbn) 
+);
+
+
+
+-- Create table "ReturnStatus"
+DROP TABLE IF EXISTS return_status;
+CREATE TABLE return_status
+(
+            return_id VARCHAR(10) PRIMARY KEY,
+            issued_id VARCHAR(30),
+            return_book_name VARCHAR(80),
+            return_date DATE,
+            return_book_isbn VARCHAR(50),
+            FOREIGN KEY (return_book_isbn) REFERENCES books(isbn)
+);
 SELECT *
 FROM books;
 SELECT *
@@ -83,10 +162,6 @@ ON b.isbn = i.issued_book_isbn
 GROUP BY 1;
 
 -- Task 9. List Members Who Registered in the Last 180 Days:
-
-SELECT * FROM members;
-
-
 
 SELECT *
 FROM members
@@ -272,7 +347,7 @@ SELECT DISTINCT(issued_member_id)
 FROM issued_status
 WHERE issued_date >= current_date() - interval  2 month);
   
-  SELECT *FROM active_members;
+  SELECT * FROM active_members;
   
   
   -- Task 17: Find Employees with the Most Book Issues Processed
@@ -311,17 +386,6 @@ HAVING
     COUNT(*) > 2;
 
 
-
-
-
-
-
-
-
-
-
-
-
 /*Task 19: Stored Procedure Objective: Create a stored procedure to manage the status of books in a library system. 
 Description: Write a stored procedure that updates the status of a book in the library based on its issuance. 
 The procedure should function as follows: The stored procedure should take the book_id as an input parameter. 
@@ -329,8 +393,6 @@ The procedure should first check if the book is available (status = 'yes').
 If the book is available, it should be issued, and the status in the books table should be updated to 'no'. 
 If the book is not available (status = 'no'), the procedure should return an error message indicating that the book is currently not available.
 */
-
-
 
 SELECT * FROM books;
 SELECT * FROM issued_status;
@@ -344,36 +406,32 @@ p_issued_book_isbn VARCHAR(30) ,
 p_issued_emp_id VARCHAR(10))
 
 BEGIN
-	-- all the variable
-	DECLARE
-	v_status VARCHAR(10);
-	-- all the code
-	-- checking if booking is availbale
+-- all the variable
+DECLARE
+v_status VARCHAR(10);
+-- all the code
+-- checking if booking is availbale
 
-		SELECT status 
-        INTO v_status 
-        FROM books WHERE isbn = p_issued_book_isbn;
+SELECT status 
+INTO v_status 
+FROM books WHERE isbn = p_issued_book_isbn;
 
-		IF v_status = 'yes' THEN 
-                -- insert the issue record
-				INSERT INTO issued_status(issued_id, issued_member_id, issued_date, issued_book_isbn, issued_emp_id)
-				VALUES(p_issued_id, p_issued_member_id, CURDATE(), p_issued_book_isbn, p_issued_emp_id);
-				-- update the books status to 'no'
-				UPDATE books
-                SET status = 'no'
-                WHERE isbn = p_issued_book_isbn;
-                -- output success message
-				SELECT CONCAT ('Book records added succesfully for isbn: ' ,  p_issued_book_isbn);
-        
+	IF v_status = 'yes' THEN 
+        -- insert the issue record
+			INSERT INTO issued_status(issued_id, issued_member_id, issued_date, issued_book_isbn, issued_emp_id)
+			VALUES(p_issued_id, p_issued_member_id, CURDATE(), p_issued_book_isbn, p_issued_emp_id);
+			-- update the books status to 'no'
+	UPDATE books
+        SET status = 'no'
+        WHERE isbn = p_issued_book_isbn;
+        -- output success message
+	SELECT CONCAT ('Book records added succesfully for isbn: ' ,  p_issued_book_isbn);
         ELSE
-				-- output not sucess message
-				SELECT CONCAT( ' Sorry the book you requested is unavilable: ' , p_issued_book_isbn);
-        
+	-- output not sucess message
+	SELECT CONCAT( ' Sorry the book you requested is unavilable: ' , p_issued_book_isbn);
         END IF ;
 END;
-
 $$
-
 CALL issue_book('IS155', 'C107', '978-0-553-29698-2', 'E104');
 CALL issue_book('IS155', 'C107', '978-0-375-41398-8', 'E104');
 
@@ -385,9 +443,7 @@ The number of books issued by each member.
 The resulting table should show: Member ID Number of overdue books Total fines
 */
 
-
 -- each member and books they issued that have not been returned within 30days
--- 
 
 CREATE TABLE overdue_books_summary AS
 SELECT 
